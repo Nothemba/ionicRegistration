23a795c8f2c61ade5274092f256180fc52d10f56
Nothemba <tnothemba@gmail.com>
2022-02-09 18:40:54 +0200

    registration

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home.page';
+import { LoginPage } from '../login/login.page';
+import { RegistrationPage } from '../registration/registration.page';
+import { ForgotPasswordPage } from '../forgot-password/forgot-password.page';
 
 const routes: Routes = [
   {
     path: '',
     component: HomePage,
+  },
+  {
+    path: 'login',
+    component: LoginPage,
+  },
+  {
+    path: 'registration',
+    component:  RegistrationPage,
+  },
+  {
+    path: 'forgot-password',
+    component:  ForgotPasswordPage,
   }
 ];
 
